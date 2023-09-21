ed19ab16496c0606efbeddbf14a7caf32ca7c0ec
Ismael Blazquez Carabias <ismatrompeta4@gmail.com>
2023-09-21 16:23:47 +0200

    Update javamavenDSL3.groovy

--- DSL/javamavenDSL3.groovy
+++ DSL/javamavenDSL3.groovy
@@ -32,10 +32,10 @@ job('Java Maven App DSL 3') {
 job('Job test Hola Mundo') {
 	description('Aplicacion Hola Mundo de Prueba')
 	scm {
-		git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-		    node / gitConfigName('macloujulian')
-		    node / gitConfigEmail('macloujulian@gmail.com')
-		}
+	        git('https://github.com/ismaBlazquez/CursoJenkins-simple-java-maven-app.git', 'master') { node ->
+	            node / gitConfigName('ismaBlazquez')
+	            node / gitConfigEmail('ismatrompeta4@gmail.com')
+	        }
 	}
 	triggers {
     		githubPush()
